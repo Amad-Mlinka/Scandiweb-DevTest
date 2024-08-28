8eff98b355d628ce70cde74e3104ea0c6f248901
Amad-Mlinka <amadovski@gmail.com>
2024-08-28 22:52:49 +0200

    Frontend/Backend - Debugging why the test is failing after clicking  save

--- scandiwebtest-frontend/src/components/Products/productsList.tsx
+++ scandiwebtest-frontend/src/components/Products/productsList.tsx
@@ -24,7 +24,6 @@ const ProductList: React.FC<ProductListProps> = ({ products, selectedProducts, o
 
     return (
         <div className="container">
-            <h1 className='text-white'>Product List</h1>
             <p className='text-white'>Product List</p>
             <hr className='text-white'/>
             <div className="row">
